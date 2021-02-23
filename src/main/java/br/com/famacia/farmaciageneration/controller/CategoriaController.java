17abebbaac58521eebfff97d970a4034966cb462
Jessica Holanda <53666998+JessicaHolanda@users.noreply.github.com>
2021-02-23 17:07:22 -0300

    ajustes na categoria

--- src/main/java/br/com/famacia/farmaciageneration/controller/CategoriaController.java
+++ src/main/java/br/com/famacia/farmaciageneration/controller/CategoriaController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.famacia.farmaciageneration.model.Categoria;
@@ -19,6 +20,7 @@ import br.com.famacia.farmaciageneration.repository.CategoriaRepository;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/categoria")
 public class CategoriaController {
 
 	@Autowired
